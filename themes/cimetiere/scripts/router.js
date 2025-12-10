caaed60bc0e9012c527fdeb504217f5ad12d1f43
hortensia <824466875@qq.com>
2025-12-10 23:17:13 +0800

    major refactor

--- themes/cimetiere/scripts/router.js
+++ themes/cimetiere/scripts/router.js
@@ -38,3 +38,69 @@ hexo.extend.generator.register(
         }
     }
 )
+
+hexo.extend.generator.register(
+    'research',
+    (locals) => {
+        return {
+            path: 'research.html',
+            data: locals.theme,
+            layout: ['research']
+        }
+    }
+)
+
+hexo.extend.generator.register(
+    'story',
+    (locals) => {
+        return {
+            path: 'story.html',
+            data: locals.theme,
+            layout: ['story']
+        }
+    }
+)
+
+hexo.extend.generator.register(
+    'essay',
+    (locals) => {
+        return {
+            path: 'essay.html',
+            data: locals.theme,
+            layout: ['essay']
+        }
+    }
+)
+
+hexo.extend.generator.register(
+    'gallery',
+    (locals) => {
+        return {
+            path: 'gallery.html',
+            data: locals.theme,
+            layout: ['gallery']
+        }
+    }
+)
+
+hexo.extend.generator.register(
+    'archive',
+    (locals) => {
+        return {
+            path: 'archive.html',
+            data: locals.theme,
+            layout: ['archive']
+        }
+    }
+)
+
+hexo.extend.generator.register(
+    'search',
+    (locals) => {
+        return {
+            path: 'search.html',
+            data: locals.theme,
+            layout: ['search']
+        }
+    }
+)
